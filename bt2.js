c331e3f0c28ca2cb1a864bdf40dcbf3469634614
nguyen suri <suringuyen2409@gmail.com>
2022-03-16 16:32:58 +0700

    fix

--- bt2.js
+++ bt2.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
 
-const timeoutScheduled = Date.now();
-
 function getPath(index, filename) {
   let path = __dirname + "\\folder\\" + index + "\\" + filename + ".txt";
   return path;
@@ -9,15 +7,13 @@ function getPath(index, filename) {
 
 async function getAllFile() {
   let promises = [];
-  let sum=0;
+  let sum = 0;
   for (let i = 0; i < 10; i++) {
     const content1 = await new Promise((resolve) =>
       setTimeout(() => {
         fs.readFile(getPath(i + 1, "file1"), "utf-8", (err, data) => {
           if (err) reject(err);
-          sum += parseInt(data);
-          resolve(sum);
-          // console.log(data);
+          resolve(parseInt(data));
         });
       }, 500)
     );
@@ -25,18 +21,17 @@ async function getAllFile() {
       setTimeout(() => {
         fs.readFile(getPath(i + 1, "file2"), "utf-8", (err, data) => {
           if (err) reject(err);
-          sum += parseInt(data);
-          resolve(sum);
-          // console.log(data);
+          resolve(parseInt(data));
         });
       }, 500)
     );
-    promises.push(content1);
-    promises.push(content2);
+    let content = content1 + content2;
+    promises.push(content);
+    Promise.all(promises).then((res) => {
+      sum += res.pop();
+    });
   }
-  return Promise.all(promises).then((res) => {
-    console.log(res[res.length-1]);
-  });
+  setTimeout(()=>console.log(sum),0);
 }
 
 getAllFile();
