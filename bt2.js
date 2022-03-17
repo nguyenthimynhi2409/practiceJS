dd5fa130f36c4a7b0d36bd8b5ae86ad04ad2202d
nguyen suri <suringuyen2409@gmail.com>
2022-03-17 15:53:03 +0700

    fix sleep

--- bt2.js
+++ bt2.js
@@ -5,30 +5,22 @@ function getPath(index, filename) {
   return path;
 }
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function getAllFile() {
-  let promises = [];
   let sum = 0;
   for (let i = 0; i < 10; i++) {
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        const content1 = fs.readFileSync(
-          getPath(i + 1, "file1"),
-          "utf-8",
-          (err, data) => {
-            if (err) throw err;
-          }
-        );
-        const content2 = fs.readFileSync(
-          getPath(i + 1, "file2"),
-          "utf-8",
-          (err, data) => {
-            if (err) throw err;
-          }
-        );
+    await sleep(1000).then(async() => {
+      await new Promise((resolve) => {
+        const content1 = fs.readFileSync(getPath(i + 1, "file1"), "utf-8");
+        const content2 = fs.readFileSync(getPath(i + 1, "file2"), "utf-8");
         resolve(Number(content1) + Number(content2));
         sum += Number(content1) + Number(content2);
-      }, 1000)
-    );
+        // console.log(sum);
+      });
+    });
   }
   console.log(sum);
 }
