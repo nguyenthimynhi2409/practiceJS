15a4943e403410ba8cff1957968661eb55d4d14d
nguyen suri <suringuyen2409@gmail.com>
2022-03-17 15:55:30 +0700

    fix sleep

--- bt2.js
+++ bt2.js
@@ -12,8 +12,8 @@ function sleep(ms) {
 async function getAllFile() {
   let sum = 0;
   for (let i = 0; i < 10; i++) {
-    await sleep(1000).then(async() => {
-      await new Promise((resolve) => {
+    await sleep(1000).then(() => {
+      return new Promise((resolve) => {
         const content1 = fs.readFileSync(getPath(i + 1, "file1"), "utf-8");
         const content2 = fs.readFileSync(getPath(i + 1, "file2"), "utf-8");
         resolve(Number(content1) + Number(content2));
