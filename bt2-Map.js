127a62f42361ed9e00b40241ddac48e12b2bf3ce
nguyen suri <suringuyen2409@gmail.com>
2022-03-18 08:36:00 +0700

    fix map

--- bt2-Map.js
+++ bt2-Map.js
@@ -12,19 +12,24 @@ function getPath(index, filename) {
 
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// const timeoutScheduled = Date.now();
+
 const getContent = async () => {
   let sum = 0;
   list.map(async (i, index) => {
-    await sleep(1000, index).then(() => {
+    await sleep(1000, index + 1).then(() => {
       return new Promise((resolve) => {
         const content1 = fs.readFileSync(getPath(i, "file1"), "utf-8");
         const content2 = fs.readFileSync(getPath(i, "file2"), "utf-8");
         resolve(Number(content1) + Number(content2));
         sum += Number(content1) + Number(content2);
+        //console.log(sum);
+        // const delay = Date.now() - timeoutScheduled;
+        // console.log(delay);
       });
     });
   });
-    setTimeout(() => console.log(sum),10000);
+  sleep(1000, 10).then(() => console.log(sum));
 };
 
 getContent();
